bdd7b4bd93f3a3b35f037130f909d8781c9e934f
pheromone <510738319@qq.com>
2019-03-13 21:08:53 +0800

    商品分类左侧导航
    商品分类左侧导航

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';  // getCategory
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category_model.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryPage extends StatefulWidget {
   final Widget child;
@@ -12,24 +13,81 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
-  void _CategoryPage() async{
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('商品分类')),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+
+// 左侧导航
+class LeftCategoryNav extends StatefulWidget {
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List list = [];
+
+ void _getCategoryData() async{
     await request('getCategory').then((val){
         var data = json.decode(val.toString());
-        CategoryModel model = CategoryModel.fromJson(data);
-        print('数据来了');
-        model.data.forEach((i){
-          print(i.mallCategoryName);
-          print(i.mallCategoryId);
+        CategoryModel categoryModel= CategoryModel.fromJson(data);
+        setState(() {
+          list = categoryModel.data;
         });
     });
   }
 
+  void initState() { 
+    super.initState();
+    _getCategoryData();
+  }
+
+  Widget _leftInkWell(int index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left:10,top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1,color: Colors.black12)
+          )
+        ),
+        child: Text(list[index].mallCategoryName,style: TextStyle(fontSize: ScreenUtil().setSp(28)),),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    _CategoryPage();
     return Container(
-       child: Text('分类页面'),
+       width: ScreenUtil().setWidth(180),
+       decoration: BoxDecoration(
+         border: Border(
+           right: BorderSide(
+             width: 1,color: Colors.black12
+           )
+         )
+       ),
+       child: ListView.builder(
+         itemCount: list.length,
+         itemBuilder: (context,index){
+           return _leftInkWell(index);
+         },
+       )
     );
   }
+  
 }
