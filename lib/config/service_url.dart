2c542faaf13181ba71feb9319be76a0c3867d529
pheromone <510738319@qq.com>
2019-03-20 22:28:33 +0800

    添加分类页面UI+模型
    添加分类页面UI+模型

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -4,4 +4,6 @@ const servicePath = {
    'homePageContent':serviceUrl + 'wxmini/homePageContent', // 商店首页信息
    'homePageBelowConten':serviceUrl + 'wxmini/homePageBelowConten', // 首页热卖商品
    'getCategory':serviceUrl + 'wxmini/getCategory',  // 商品类别信息
+   'getMallGoods':serviceUrl + 'wxmini/getMallGoods',  // 商品分类页面商品列表
+
 };
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart'; // getCategory
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category_model.dart';
+import '../model/categoryGoodsList.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provide/provide.dart';
 import '../provide/child_category.dart';
@@ -26,6 +27,7 @@ class _CategoryPageState extends State<CategoryPage> {
               Column(
                 children: <Widget>[
                   RightCategoryNav(),
+                  CategoryGoodsList(),
                 ],
               )
             ],
@@ -148,4 +150,123 @@ class _LeftCategoryNavState extends State<LeftCategoryNav> {
           },
         ));
   }
+}
+
+// 商品列表 
+class CategoryGoodsList extends StatefulWidget {
+  @override
+  _CategoryGoodsListState createState() => _CategoryGoodsListState();
+}
+
+class _CategoryGoodsListState extends State<CategoryGoodsList> {
+  List list = [];
+  @override
+  void initState() {
+    super.initState();
+    _getMallGoods();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(570),
+      height: ScreenUtil().setHeight(970),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context,index){
+            return _goodsItem(index);
+        },
+      )
+    );
+  }
+
+  
+  // 商品图片
+  Widget _goodsImage(index){
+    return Container(
+      width: ScreenUtil().setWidth(200),
+      child: Image.network(list[index].image),
+    );
+  }
+  // 商品名字
+  Widget _goodsName(index){
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      width: ScreenUtil().setWidth(370),
+       child: Text(
+       list[index].goodsName,
+       maxLines: 2,
+       overflow: TextOverflow.ellipsis,
+       style: TextStyle(fontSize: ScreenUtil().setSp(28)),  
+       ),
+    );
+  }
+  // 价格
+  Widget _goodsPrice(index){
+    return Container(
+      width: ScreenUtil().setWidth(370),
+      margin: EdgeInsets.only(top: 20.0),
+      child: Row(
+        children: <Widget>[
+          Text(
+            '价格:¥${list[index].presentPrice}',
+            style: TextStyle(color: Colors.pink,fontSize: ScreenUtil().setSp(30)),
+            ),
+          Text(
+            '¥${list[index].oriPrice}',
+             style: TextStyle(color:Colors.black26,decoration: TextDecoration.lineThrough),
+            )
+        ],
+      ),
+    );
+  } 
+  // 商品item
+  Widget _goodsItem(int index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 5.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.black12,
+              width: 1
+            )
+          )
+        ),
+        child: Row(
+          children: <Widget>[
+            _goodsImage(index),
+            Column(
+              children: <Widget>[
+                _goodsName(index),
+                _goodsPrice(index),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+   void _getMallGoods() async{
+      var data = {
+        'categoryId':'4',
+        'categorySubId':'',
+        'page':1
+      };
+
+      request('getMallGoods',formData:data).then((val){
+        var data = json.decode(val.toString());
+        CategoryGoodsListModel goodsList =CategoryGoodsListModel.fromJson(data);
+        setState(() {
+          list = goodsList.data;
+        });
+      }); 
+
+   }
+
 }
