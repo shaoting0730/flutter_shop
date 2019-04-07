23d4528420833f0b9958aa8260f46d7a8be3a909
pheromone <510738319@qq.com>
2019-04-07 17:26:57 +0800

    商品详情界面
    商品详情界面

--- lib/pages/details_page/details_web.dart
+++ lib/pages/details_page/details_web.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import '../../provide/details_info.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_html/flutter_html.dart';
 
 class DetailsWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var goodsDetails = Provide.value<DetailsInfoProvide>(context).goodsInfo.data.goodInfo.goodsDetail;
-    return Container(
-      child: Html(
-        data: goodsDetails,
-      ),
+    var goodsDetails = Provide.value<DetailsInfoProvide>(context)
+        .goodsInfo
+        .data
+        .goodInfo
+        .goodsDetail;
+    return Provide<DetailsInfoProvide>(
+      builder: (context, child, val) {
+        var isLeft = Provide.value<DetailsInfoProvide>(context).isLeft;
+        if (isLeft) {
+         return  Container(
+            child: Html(
+              data: goodsDetails,
+            ),
+          );
+        } else {
+          return Container(
+            width: ScreenUtil().setWidth(750),
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            child: Text('暂时没有数据'),
+          );
+        }
+      },
     );
   }
 }
