55058802032508014a4ad72b502375a7a9c7d0df
pheromone <510738319@qq.com>
2019-04-13 11:12:36 +0800

    购物车基本UI
    购物车基本UI

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+import '../provide/cart.dart';
+import 'dart:async';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('购物车'),
       ),
+      body: FutureBuilder(
+        future: _getCartInfo(context),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            List castList = Provide.value<CartProvide>(context).cartList;
+            return ListView.builder(
+              itemCount:castList.length,
+              itemBuilder: (context,index){
+                return ListTile(
+                  title: Text(castList[index].goodsName),
+                );
+              },
+            );
+          }else{
+              return Text('正在加载');
+          }
+        },
+      )
     );
   }
+ 
+   Future<String> _getCartInfo(BuildContext context) async{
+     await Provide.value<CartProvide>(context).getCartInfo();
+     return 'end';
+   }
+  
 }
